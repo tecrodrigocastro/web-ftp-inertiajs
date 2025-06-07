1013966bea9ab0df7845a93ceab945131f14ee58
Rodrigo Castro <tec.rodrigocastro@gmail.com>
2025-06-07 13:49:22 -0300

    feat: replace Laravel welcome with WebFTP app layout

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <title>KingWebFTP</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @inertiaHead
+  </head>
+  <body class="bg-gray-50">
+    @inertia
+  </body>
+</html>
