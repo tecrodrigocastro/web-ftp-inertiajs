976f4a6a651307c99f676e2fef868b574a476a19
Rodrigo Castro <tec.rodrigocastro@gmail.com>
2025-06-07 13:47:51 -0300

    feat: add FTP configuration file

---
+++ config/ftp.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'host' => env('FTP_HOST', '93.127.190.164'),
+    'username' => env('FTP_USERNAME', 'your_username'),
+    'password' => env('FTP_PASSWORD', 'your_password'),
+    'port' => (int) env('FTP_PORT', 21),
+    'root' => env('FTP_ROOT', '/'),
+    'passive' => env('FTP_PASSIVE', true),
+    'ssl' => env('FTP_SSL', false),
+    'timeout' => env('FTP_TIMEOUT', 30),
+];
